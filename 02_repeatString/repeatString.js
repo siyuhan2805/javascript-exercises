009429602e77b180605c28c77d7f885b3e9e5797
Henry <siyu.han2805@gmail.com>
2024-06-04 00:15:54 +1000

    Complete exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    if (num < 0) {
+        return "ERROR"
+    }
+    else {
+        return word.repeat(num);
+    }
+    
 };
+
+console.log(repeatString("hey", -3))
 
 // Do not edit below this line
 module.exports = repeatString;
