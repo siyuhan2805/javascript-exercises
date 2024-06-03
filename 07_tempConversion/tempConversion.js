f169ea6ab09be49bcdd7c3f466153202b04b9ea3
Henry <siyu.han2805@gmail.com>
2024-06-04 03:41:08 +1000

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  //formula for converting Fahrenheit (F) to Celsius (C): (F - 32) x 5/9
+
+  //rounding to 1 dp
+  return Math.round(((tempF - 32) * 5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  //formula for converting Celsius (C) to Fahrenheit (F): (C x 9/5 + 32)
+
+  //rounding to 1 dp
+  return Math.round((tempC * 9/5 + 32) * 10) / 10;
 };
+
+
 
 // Do not edit below this line
 module.exports = {
