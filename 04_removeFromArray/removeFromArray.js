61d8d51a2f47c3fdb2324cc8e5dbec7bb3c17a8b
Henry <siyu.han2805@gmail.com>
2024-06-04 02:48:09 +1000

    Complete exercises 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    //array to hold filtered items from arr that are not in args
+    const filteredArr = [];
+    //iterate over each item in arr and check if args has said item 
+    arr.forEach((item) => {;
+        //items from arr not present in args are pushed to filteredArr 
+        if (!args.includes(item)) {
+            filteredArr.push(item);
+        }
+    })
+
+    return filteredArr;
+    
 };
 
 // Do not edit below this line
