553409757d5f7a043d52d41f48d5e07670e6b48b
Henry <siyu.han2805@gmail.com>
2024-06-04 03:23:36 +1000

    Complete exercises 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,26 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    
+    //lea year conditions: years that are divisible by 4, however, years divisible by 100 are not leap years (unless they are also divisible by 400)
 
+    //highest order conditional check for a leap year - years that dont pass are automatically non-leap years
+    if (year % 4 === 0) {
+        //edge case testing
+        if (year % 100 === 0 && year % 400 != 0) {
+            return false;
+        }
+        else if (year % 100 === 0 && year % 400 === 0) {
+            return true;
+        }
+        //years that fail the edge case are leap years
+        else {
+            return true;
+        }
+    }
+    else {
+        return false;
+    }
 };
+
 
 // Do not edit below this line
 module.exports = leapYears;
