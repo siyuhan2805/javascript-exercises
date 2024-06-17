12e32ba917e90510f3f4de8059980d510133a0fa
Henry <siyu.han2805@gmail.com>
2024-06-17 22:35:17 +1000

    Complete exercises 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    const bookTitles = books.map(book => book.title);
+    return bookTitles;
 
 };
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
