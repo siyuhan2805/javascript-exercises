4ce728a1bb2165c397527347337a09b41ccdb3b0
Henry <siyu.han2805@gmail.com>
2024-06-17 21:07:10 +1000

    Complete exercises 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, curr) => {
+    return total = total + curr;
+  }, 0);
 };
 
-const multiply = function() {
 
+const multiply = function(arr) {
+	return arr.reduce((total, curr) => {
+    return total = total * curr;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1;
+  }
+  let product = 1;
+  for (let i = num; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
