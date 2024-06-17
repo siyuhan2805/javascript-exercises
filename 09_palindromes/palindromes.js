7f3c07e02960cb196e1d59a8a8e3e2761e3aa2cb
Henry <siyu.han2805@gmail.com>
2024-06-17 21:47:23 +1000

    Complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    //split each character into an array
+    const chars = string.toLowerCase().split("");
+    //filter the non-alphanumeric chars out for
+    const filtered = chars.filter(char => {
+        if (char.charCodeAt() >= 48 && char.charCodeAt() <= 57 || char.charCodeAt() >= 97 && char.charCodeAt() <= 122) {
+            return char
+        }
+    })
 
+    //recombine the filtered chars
+    cleanedString = filtered.join("");
+    //recombined reversed filtered chars
+    revCleanedString = filtered.reverse().join("");
+    if (cleanedString === revCleanedString) {
+        return true
+    }
+    else {
+        return false;
+    }
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
